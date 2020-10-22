4fc35e95329440ee2f81322c25e6e079ff9b6839
Dinesh Kanchi <k.d.v.kumar123@gmail.com>
2020-10-23 01:15:35 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React, { Component, useState } from "react";
 import "./../styles/App.css";
-import Paragraph from "./Paragraph";
 export default function App() {
+  const [click, setClick] = React.useState(false);
+  let isClicked = () => {
+    let copyClick = click;
+    copyClick = true;
+    setClick(copyClick);
+  };
   return (
     <div id="main">
-      <Paragraph />
+      <button id="click" onClick={() => isClicked()}>
+        Click
+      </button>
+      {click && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
     </div>
   );
 }
